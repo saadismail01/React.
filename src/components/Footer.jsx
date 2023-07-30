fcee735385432050d9246abec4a12075e11b37d0
Saad <saadismail001@gmail.com>
2023-07-30 21:33:25 +0300

    Update: fixing Footer in the responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ import {
 
 const Footer = () => {
   return (
-    <div className="max-w-[1240px] mx-auto py-16 px-4 text-gray-300 flex justify-between items-end">
+    <div className="lg:flex justify-between max-w-[1240px] mx-auto py-16 px-4 text-gray-300 ">
       <div className="flex flex-col w-72">
         <h1 className="w-full text-3xl font-bold text-[#00d9fa] my-2">
           REACT.
@@ -24,18 +24,30 @@ const Footer = () => {
           <FaTwitterSquare size={30} />
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-3 space-x-20">
-        <div className="w-40">
-          Lorem Loremghjkl hjkl hjkl jkl nmkl bnm bnm bnk bnk bnjk bnmk bnmk
-          bnmk bnmk bnmk bnm b nm bnm
+      <div className="lg:col-span-2 flex justify-between w-2/4 mt-10">
+        <div>
+          <ul>
+            <li>fghj</li>
+            <li>ghjkl</li>
+            <li>ghjkk</li>
+            <li>fghjk</li>
+          </ul>
         </div>
-        <div className="w-40">
-          Lorem Loremghjkl hjkl hjkl jkl nmkl bnm bnm bnk bnk bnjk bnmk bnmk
-          bnmk bnmk bnmk bnm b nm bnm
+        <div>
+          <ul>
+            <li>fghj</li>
+            <li>ghjkl</li>
+            <li>ghjkk</li>
+            <li>fghjk</li>
+          </ul>
         </div>
-        <div className="w-40">
-          Lorem Loremghjkl hjkl hjkl jkl nmkl bnm bnm bnk bnk bnjk bnmk bnmk
-          bnmk bnmk bnmk bnm b nm bnm
+        <div>
+          <ul>
+            <li>fghj</li>
+            <li>ghjkl</li>
+            <li>ghjkk</li>
+            <li>fghjk</li>
+          </ul>
         </div>
       </div>
     </div>
